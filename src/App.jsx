3278dd44f6cdac04f33fe679887cd0fec12fd27f
mohsen hesham <mohsenhesham.110@gmail.com>
2025-09-02 11:01:21 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <div>
+      <h1>My React App L1</h1>
+      <Home />
+      <About />
+      <Contact />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contact.jsx
@@ -0,0 +1,11 @@
+function Contact() {
+    return (
+      <div>
+        <h2>Contact Component</h2>
+        <p>This is some static contact text.</p>
+      </div>
+    );
+  }
+  
+  export default Contact;
+  
